d9ecd635e4990a74ddaf6aea8479d4342bbbef78
Ezequiel <ki3lmigu3l@gmail.com>
2023-11-21 11:05:56 -0300

    Controller de Food adicionado ao projeto

---
+++ food/src/main/java/com/github/ki3lmigu3l/food/controller/FoodController.java
@@ -0,0 +1,11 @@
+package com.github.ki3lmigu3l.food.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(name = "food")
+public class FoodController {
+
+
+}
